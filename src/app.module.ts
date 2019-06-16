3db87f611b09605f7785a8beefad44887a32a663
key Kim <keyhyuk.kim@gmail.com>
2019-06-16 11:06:09 +0000

    Add about-me service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AboutMeController } from './about-me/about-me.controller';
+import { AboutMeService } from './about-me/about-me.service';
 
 @Module({
   imports: [],
   controllers: [AppController, AboutMeController],
-  providers: [AppService],
+  providers: [AppService, AboutMeService],
 })
 export class AppModule {}
